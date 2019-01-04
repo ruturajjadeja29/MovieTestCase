9f211518a54f70d5a7cc1b92d48caf7713858f24
mac-0009 <mac-0009@example.com>
2019-01-04 18:29:47 +0530

    - Added MovieList Table Cell

---
+++ Movie/Controller/Cell/MovieListTblCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieListTblCell.swift
+//  Movie
+//
+//  Created by mac-0009 on 04/01/19.
+//  Copyright © 2019 mac-0009. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTblCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
